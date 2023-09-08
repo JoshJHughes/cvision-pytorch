6a29fb08d84c19b91ea212f55692abe6457a7134
JoshJHughes <JoshJHughes@users.noreply.github.com>
2023-09-08 18:56:53 +0100

    add script to test forward method on pennfudandataset

---
+++ scripts/torchvision_object_detection_tutorial/script.py
@@ -0,0 +1,67 @@
+import torch
+import torchvision
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+from references.detection import utils
+from references.detection import transforms as T
+from references.detection import engine
+
+from cvision.datasets.PennFudanDataset import PennFudanDataset
+
+def get_model_instance_segmentation(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(
+        in_features_mask,
+        hidden_layer,
+        num_classes,
+    )
+
+    return model
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.PILToTensor())
+    transforms.append(T.ToDtype(torch.float, scale=True))
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+def test_forward_method():
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+    dataset = PennFudanDataset('data/PennFudanPed', get_transform(train=True))
+    data_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=2,
+        shuffle=True,
+        num_workers=4,
+        collate_fn=utils.collate_fn
+    )
+
+    # For Training
+    images, targets = next(iter(data_loader))
+    images = list(image for image in images)
+    targets = [{k: v for k, v in t.items()} for t in targets]
+    output = model(images, targets)  # Returns losses and detections
+    print(output)
+
+    # For inference
+    model.eval()
+    x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+    predictions = model(x)  # Returns predictions
+    print(predictions[0])
+
+if __name__ == "__main__":
+    test_forward_method()
