352d00d5dba15158a870ae6ee0e025dd007549aa
JoshJHughes <JoshJHughes@users.noreply.github.com>
2023-09-18 16:24:02 +0100

    modified lenet_custom to use tanh activation fn

--- cvision/classifiers/lenet_custom.py
+++ cvision/classifiers/lenet_custom.py
@@ -4,21 +4,32 @@ class Lenet_Custom(nn.Module):
     def __init__(self, num_classes=10):
         super().__init__()
         self.num_classes = num_classes
-        self.net = nn.Sequential(
+        self.activation_fn = nn.Tanh
+        self.pool_fn = nn.AvgPool2d
+
+        self.features = nn.Sequential(
             nn.LazyConv2d(out_channels=6, kernel_size=5, padding=2),
-            nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            self.activation_fn(),
+            self.pool_fn(kernel_size=2, stride=2),
+
             nn.LazyConv2d(out_channels=16, kernel_size=5),
-            nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            self.activation_fn(),
+            self.pool_fn(kernel_size=2, stride=2),
+        )
+
+        self.classifier= nn.Sequential(
             nn.Flatten(),
+
             nn.LazyLinear(out_features=120),
-            nn.Sigmoid(),
+            self.activation_fn(),
+
             nn.LazyLinear(out_features=84),
-            nn.Sigmoid(),
-            nn.LazyLinear(out_features=num_classes)
+            self.activation_fn(),
+
+            nn.LazyLinear(out_features=num_classes),
         )
 
     def forward(self, x):
-        logits = self.net(x)
+        net = nn.Sequential(self.features, self.classifier)
+        logits = net(x)
         return logits
