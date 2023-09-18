c590a6ba05a9533ac49569b5b5440eb46547fc76
JoshJHughes <JoshJHughes@users.noreply.github.com>
2023-09-18 16:25:12 +0100

    modified to use lenet_custom, removed image resize

--- scripts/lenet_custom_MNIST/script.py
+++ scripts/lenet_custom_MNIST/script.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
 from torchvision import datasets
@@ -8,30 +6,6 @@ import torchvision.transforms as T
 
 import matplotlib.pyplot as plt
 from cvision.classifiers.lenet_custom import Lenet_Custom
-
-class Net(nn.Module):
-
-    def __init__(self):
-        super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square, you can specify with a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 def visualise(imgs):
     figure = plt.figure(figsize=(8, 8))
@@ -47,7 +21,6 @@ def visualise(imgs):
 
 def get_transforms():
     transforms = []
-    transforms.append(T.Resize(32))
     transforms.append(T.ToTensor())
     return T.Compose(transforms)
 
@@ -137,8 +110,8 @@ def main():
     test_dl = DataLoader(test_ds, batch_size=hp_batch_size, shuffle=True)
 
     # get model & move to gpu
-    # model = Lenet_Custom(n_classes)
-    model = Net()
+    model = Lenet_Custom(n_classes)
+    # model = Net()
     model.to(device)
 
     # construct an optimizer
